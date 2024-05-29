d58d80628e3466714f3537d336b2348d50028816
DoDoBest <send1684@gmail.com>
2024-05-29 21:57:55 +0900

    feat: Navigation Component 설정
    Co-authored-by: rlaxodud214 <rlaxodud214@naver.com>
    Co-authored-by: yoonchanchoi <choyc9239@gmail.com>
    Co-authored-by: kory0115 <kory01167@gmail.com>

--- app/src/main/java/com/bestapp/rice/FireBaseMessageReceiver.kt
+++ app/src/main/java/com/bestapp/rice/FireBaseMessageReceiver.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import androidx.core.app.NotificationCompat
+import com.bestapp.rice.ui.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
