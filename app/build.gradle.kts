f12eda691fd7c0c90e9510d6b799b12003fec372
rlaxodud214 <rlaxodud214@naver.com>
2024-05-29 17:31:48 +0900

    init project
    Co-authored-by: DoTheBestMayB <send1684@gmail.com>
    Co-authored-by: yoonchanchoi <choyc9239@gmail.com>
    Co-authored-by: kory0115 <kory01167@gmail.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.navigationSafeArgs)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinParcelize)
+    alias(libs.plugins.googleService)
+}
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
+
+android {
+    namespace = "com.bestapp.rice"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.bestapp.rice"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "SOME_KEY", "${properties["some_key"]}")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.storage)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args)
+    }
+}
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.navigationSafeArgs) apply false
+    alias(libs.plugins.kotlinParcelize) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.googleService) apply false
+}
