f9ff5872747721d13eeabf17e1ccbddc56808b91
kory0115 <kory01167@gmail.com>
2024-05-30 18:49:23 +0900

    fix: test

--- app/src/main/java/com/bestapp/rice/ui/login/LoginFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/login/LoginFragment.kt
@@ -1,7 +1,16 @@
 package com.bestapp.rice.ui.login
 
+import android.os.Bundle
+import android.view.View
 import com.bestapp.rice.databinding.FragmentLoginBinding
 import com.bestapp.rice.ui.BaseFragment
 
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate)
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
