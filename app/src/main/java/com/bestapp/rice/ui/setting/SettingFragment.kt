50c34486df2b0784a3a57013bc3da049a52877e4
DoDoBest <send1684@gmail.com>
2024-05-30 19:45:20 +0900

    feat: 설정 화면 xml 설정

--- app/src/main/java/com/bestapp/rice/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/setting/SettingFragment.kt
@@ -1,6 +1,20 @@
 package com.bestapp.rice.ui.setting
 
+import android.os.Bundle
+import android.view.View
 import com.bestapp.rice.databinding.FragmentSettingBinding
 import com.bestapp.rice.ui.BaseFragment
 
-class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate)
+class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUI()
+    }
+
+    private fun setUI() {
+        binding.ivProfileImage.clipToOutline = true
+    }
+}
