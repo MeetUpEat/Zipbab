04710af579a36fb41b95a04bbab74ea565bf62dc
DoDoBest <send1684@gmail.com>
2024-05-30 21:41:14 +0900

    feat: 설정 버튼 UI 구현

--- app/src/main/java/com/bestapp/rice/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/setting/SettingFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import coil.load
+import com.bestapp.rice.BuildConfig
 import com.bestapp.rice.R
 import com.bestapp.rice.databinding.FragmentSettingBinding
 import com.bestapp.rice.model.UserUiState
@@ -28,13 +29,34 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setViewAttribute()
+        setDefaultUI()
         setObserve()
     }
 
-    private fun setViewAttribute() {
+    private fun setDefaultUI() {
         // minSdk로 인해 xml attribute가 아닌 코드에서 설정함
         binding.ivProfileImage.clipToOutline = true
+
+        binding.viewProfile.tvTitle.text = "프로필 수정"
+        binding.viewProfile.tvDescription.text = "프로필을 수정합니다."
+        binding.viewProfile.ivIcon.setImageResource(R.drawable.baseline_person_24)
+
+        binding.viewMeeting.tvTitle.text = "모임"
+        binding.viewMeeting.tvDescription.text = "참가중인 모임과 참여했던 모임을 확인합니다."
+        binding.viewMeeting.ivIcon.setImageResource(R.drawable.baseline_people_24)
+
+        binding.viewAlert.tvTitle.text = "알림 설정"
+        binding.viewAlert.tvDescription.text = "개별적인 알림을 설정합니다."
+        binding.viewAlert.ivIcon.setImageResource(R.drawable.baseline_notifications_none_24)
+
+        binding.viewPrivacyPolicy.tvTitle.text = "개인정보 처리방침"
+        binding.viewPrivacyPolicy.tvDescription.text = "개인정보 처리방침을 확인합니다."
+        binding.viewPrivacyPolicy.ivIcon.setImageResource(R.drawable.baseline_remove_red_eye_24)
+
+        binding.viewVersion.tvTitle.text = "버전"
+        binding.viewVersion.tvDescription.text = getString(R.string.version_format).format(BuildConfig.VERSION_NAME)
+        binding.viewVersion.ivIcon.setImageResource(R.drawable.baseline_code_24)
+        binding.viewVersion.ivEnter.visibility = View.GONE
     }
 
     private fun setObserve() {
