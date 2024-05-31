9cae0bd480de72d7723d209a248f6f1d2980b863
rlaxodud214 <rlaxodud214@naver.com>
2024-05-31 15:21:28 +0900

    add: api call query의 Int 범위 제한

--- data/src/main/java/com/bestapp/rice/data/repository/SearchLocationRepository.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/SearchLocationRepository.kt
@@ -1,12 +1,13 @@
 package com.bestapp.rice.data.repository
 
+import androidx.annotation.IntRange
 import com.bestapp.rice.data.model.remote.kakaomap.SearchLocation
 
 interface SearchLocationRepository {
     suspend fun convertLocation(
         query: String,
         analyze_type: String = "similar",
-        page: Int = 1,
-        size: Int = 10,
+        @IntRange(from = 1, to = 45) page: Int = 1,
+        @IntRange(from = 1, to = 30) size: Int = 10,
     ): SearchLocation
 }
