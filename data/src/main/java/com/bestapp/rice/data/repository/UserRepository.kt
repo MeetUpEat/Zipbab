ce4649a279b28ed2d085e33473de2e0e1d21a799
DoDoBest <send1684@gmail.com>
2024-05-31 11:09:05 +0900

    chore: UserRepository에 회원탈퇴 함수 추가

--- data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
@@ -10,6 +10,7 @@ interface UserRepository {
     suspend fun getUser(userDocumentId: String): User
     suspend fun login(): User
     suspend fun signUp(user: User)
+    suspend fun signOut(user: User): Boolean
     suspend fun updateUserTemperature(reviews: List<Review>)
     suspend fun updateUserMeetingCount()
     suspend fun updateUserProfileImage(userID: String, profileImageUri: String?)
