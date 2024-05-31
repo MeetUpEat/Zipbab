8042d14085b32ded34f6efb41b95623074b10d17
Choi YoonChan <choyc9239@gmail.com>
2024-06-01 04:46:37 +0900

    feat: foodmenu adapter 구현

---
+++ app/src/main/java/com/bestapp/rice/ui/home/recyclerview/FoodMenuViewHolder.kt
@@ -0,0 +1,35 @@
+package com.bestapp.rice.ui.home.recyclerview
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.bestapp.rice.R
+import com.bestapp.rice.databinding.ItemFoodMenuBinding
+import com.bestapp.rice.model.FilterUiState
+
+class FoodMenuViewHolder(
+    parent: ViewGroup,
+    private val onFoodMenuClick: (foodUiState: FilterUiState.FoodUiState) -> Unit,
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_food_menu, parent, false)
+) {
+
+    private val binding = ItemFoodMenuBinding.bind(itemView)
+
+    fun bind(
+        foodUiState: FilterUiState.FoodUiState
+    ) {
+        binding.iv.load(R.drawable.ic_launcher_background) {
+            crossfade(true)
+            placeholder(R.drawable.shape_et_round_bg)
+            transformations(RoundedCornersTransformation())
+        }
+        binding.tv.text = foodUiState.name
+        binding.root.setOnClickListener {
+            onFoodMenuClick(foodUiState)
+        }
+    }
+}
