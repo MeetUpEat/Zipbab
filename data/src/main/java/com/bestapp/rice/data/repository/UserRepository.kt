d560b65ed254cfaadae696ab425b3aa337bc4fd8
DoDoBest <send1684@gmail.com>
2024-05-31 11:05:22 +0900

    chore: UserRepository 오타 수정

--- data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
@@ -9,7 +9,7 @@ import com.bestapp.rice.data.model.remote.User
 interface UserRepository {
     suspend fun getUser(userDocumentId: String): User
     suspend fun login(): User
-    suspend fun signUpUs(user: User)
+    suspend fun signUp(user: User)
     suspend fun updateUserTemperature(reviews: List<Review>)
     suspend fun updateUserMeetingCount()
     suspend fun updateUserProfileImage(userID: String, profileImageUri: String?)
