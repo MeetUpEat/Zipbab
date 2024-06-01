41b410ea8c3dfe60faa3e4879c833bca82eb84e1
DoDoBest <send1684@gmail.com>
2024-06-01 19:51:07 +0900

    chore: 사용자 온도 관련 클래스에서 사용하지 않는 파라미터 삭제

--- app/src/main/java/com/bestapp/rice/model/UserTemperature.kt
+++ app/src/main/java/com/bestapp/rice/model/UserTemperature.kt
@@ -5,14 +5,13 @@ import com.bestapp.rice.R
 
 enum class UserTemperature(
     val minTemperature: Double,
-    val emoji: String,
     @ColorRes val colorRes: Int
 ) {
-    LOW(0.0, "\uD83D\uDE21", R.color.temperature_min_0),
-    NORMAL(30.0, "\uD83D\uDE42", R.color.temperature_min_30),
-    GOOD(40.0, "\uD83D\uDE0A", R.color.temperature_min_40),
-    BETTER(60.0, "\uD83D\uDE0A", R.color.temperature_min_60),
-    EXCELLENT(80.0, "\uD83D\uDE04", R.color.temperature_min_80);
+    LOW(0.0, R.color.temperature_min_0),
+    NORMAL(30.0, R.color.temperature_min_30),
+    GOOD(40.0, R.color.temperature_min_40),
+    BETTER(60.0, R.color.temperature_min_60),
+    EXCELLENT(80.0, R.color.temperature_min_80);
 
     companion object {
         fun from(temper: Double): UserTemperature {
