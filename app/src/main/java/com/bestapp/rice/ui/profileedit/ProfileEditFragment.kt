6d4cc4db914ad8b3195f79c1171d884ac13b0b79
DoDoBest <send1684@gmail.com>
2024-06-01 22:48:18 +0900

    fix: ProfileEditFragment에 잘못 설정한 binding 수정

--- app/src/main/java/com/bestapp/rice/ui/profileedit/ProfileEditFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/profileedit/ProfileEditFragment.kt
@@ -1,6 +1,7 @@
 package com.bestapp.rice.ui.profileedit
 
-import com.bestapp.rice.databinding.FragmentProfileBinding
+import com.bestapp.rice.databinding.FragmentProfileEditBinding
 import com.bestapp.rice.ui.BaseFragment
 
-class ProfileEditFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate)
+class ProfileEditFragment :
+    BaseFragment<FragmentProfileEditBinding>(FragmentProfileEditBinding::inflate)
