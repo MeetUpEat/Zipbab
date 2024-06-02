3e0e8e453af4d7f607a020059e0796d70176c95d
DoDoBest <send1684@gmail.com>
2024-06-02 20:29:14 +0900

    feat: UI 구현

--- app/src/main/java/com/bestapp/rice/ui/review/ReviewFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/review/ReviewFragment.kt
@@ -1,5 +1,7 @@
 package com.bestapp.rice.ui.review
 
+import android.os.Bundle
+import android.view.View
 import com.bestapp.rice.databinding.FragmentReviewBinding
 import com.bestapp.rice.ui.BaseFragment
 
