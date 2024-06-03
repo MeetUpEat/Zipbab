b8f4ffb66252ec7ab12ab960d0cb078c5b7aea2c
rlaxodud214 <rlaxodud214@naver.com>
2024-06-03 12:55:30 +0900

    feat: MeetingRepository 구현 및 파라미터 수정

--- data/src/main/java/com/bestapp/rice/data/repository/MeetingRepository.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/MeetingRepository.kt
@@ -11,18 +11,18 @@ interface MeetingRepository {
 
     suspend fun getCostMeeting(costType: Int): List<Meeting>
 
-    suspend fun createMeeting(mainMenu: String): List<Meeting>
+    suspend fun createMeeting(meeting: Meeting)
 
-    suspend fun updateAttendanceCheckMeeting(meetingDocumentID: String)
+    suspend fun updateAttendanceCheckMeeting(meetingDocumentID: String, userDocumentId: String)
 
     suspend fun endMeeting(meetingDocumentID: String)
 
     // 참여 대기중인 멤버리스트에 신청자 추가하기
-    suspend fun addPendingMember(meetingDocumentID: String)
+    suspend fun addPendingMember(meetingDocumentID: String, userDocumentId: String)
 
     // 참여 대기중인 멤버를 참여자 리스트로 옮겨주기
-    suspend fun approveMember(meetingDocumentID: String)
+    suspend fun approveMember(meetingDocumentID: String, userDocumentId: String)
 
     // pendingmembers 리스트에서 해당 멤버를 제거하기
-    suspend fun rejectMember(meetingDocumentID: String)
+    suspend fun rejectMember(meetingDocumentID: String, userDocumentId: String)
 }
--- data/src/main/java/com/bestapp/rice/data/repository/MeetingRepositoryImpl.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/MeetingRepositoryImpl.kt
@@ -1,28 +1,126 @@
 package com.bestapp.rice.data.repository
 
 import com.bestapp.rice.data.model.remote.Meeting
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
 
-interface MeetingRepository {
-    suspend fun getMeeting(meetingDocumentID: String): List<Meeting>
+class MeetingRepositoryImpl(
+    private val meetingDB: CollectionReference,
+) : MeetingRepository {
+    override suspend fun getMeeting(meetingDocumentID: String): List<Meeting> {
+        val querySnapshot = meetingDB
+            .whereEqualTo("meetingDocumentID", meetingDocumentID)
+            .get()
+            .await()
 
-    suspend fun getSearch(meetingDocumentID: String): List<Meeting>
+        return querySnapshot.toMeetings()
+    }
 
-    suspend fun getFoodMeeting(mainMenu: String): List<Meeting>
+    /**
+     * @param query 검색어(띄워쓰기 인식 가능)
+     */
+    override suspend fun getSearch(query: String): List<Meeting> {
+        val querySnapshot = meetingDB
+            .whereArrayContains("title", query.split(" "))
+            .get()
+            .await()
 
-    suspend fun getCostMeeting(costType: Int): List<Meeting>
+        return querySnapshot.toMeetings()
+    }
 
-    suspend fun createMeeting(mainMenu: String): List<Meeting>
+    override suspend fun getFoodMeeting(mainMenu: String): List<Meeting> {
+        val querySnapshot = meetingDB
+            .whereEqualTo("mainMenu", mainMenu)
+            .get()
+            .await()
 
-    suspend fun updateAttendanceCheckMeeting(meetingDocumentID: String)
+        return querySnapshot.toMeetings()
+    }
 
-    suspend fun endMeeting(meetingDocumentID: String)
+    override suspend fun getCostMeeting(costType: Int): List<Meeting> {
+        val querySnapshot = meetingDB
+            .whereEqualTo("costTypeByPerson", costType)
+            .get()
+            .await()
+
+        return querySnapshot.toMeetings()
+    }
+
+    /**
+     * 미팅 추가 및 meetingDocumentID 업데이트 로직 추가
+     */
+    override suspend fun createMeeting(meeting: Meeting) {
+        val documentRef = meetingDB
+            .add(meeting)
+            .await()
+
+        val meetingDocumentID = documentRef.id
+
+        meetingDB.document(meetingDocumentID)
+            .update("meetingDocumentID", meetingDocumentID)
+            .await()
+    }
+
+    override suspend fun updateAttendanceCheckMeeting(meetingDocumentID: String, userDocumentId: String) {
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "attendanceCheck",
+                FieldValue.arrayUnion(userDocumentId)
+            ).await()
+    }
+
+    override suspend fun endMeeting(meetingDocumentID: String) {
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "activation",
+                false
+            ).await()
+    }
 
     // 참여 대기중인 멤버리스트에 신청자 추가하기
-    suspend fun addPendingMember(meetingDocumentID: String)
+    override suspend fun addPendingMember(meetingDocumentID: String, userDocumentId: String) {
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "pendingMembers",
+                FieldValue.arrayUnion(userDocumentId)
+            ).await()
+    }
 
     // 참여 대기중인 멤버를 참여자 리스트로 옮겨주기
-    suspend fun approveMember(meetingDocumentID: String)
+    override suspend fun approveMember(meetingDocumentID: String, userDocumentId: String) {
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "pendingMembers",
+                FieldValue.arrayRemove(userDocumentId)
+            ).await()
+
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "members",
+                FieldValue.arrayUnion(userDocumentId)
+            ).await()
+    }
 
     // pendingmembers 리스트에서 해당 멤버를 제거하기
-    suspend fun rejectMember(meetingDocumentID: String)
+    override suspend fun rejectMember(meetingDocumentID: String, userDocumentId: String) {
+        meetingDB.document(meetingDocumentID)
+            .update(
+                "pendingMembers",
+                FieldValue.arrayRemove(userDocumentId)
+            ).await()
+    }
+
+    private fun QuerySnapshot.toMeetings(): List<Meeting> {
+        val meetings = ArrayList<Meeting>(this.documents.size)
+
+        forEachIndexed { i, document ->
+            meetings[i] = document.toObject<Meeting>()
+        }
+
+        return meetings.toList()
+    }
 }
