73aba5687d7fd47e29f7f0da3a0fc8db143012b3
Choi YoonChan <choyc9239@gmail.com>
2024-06-03 22:53:05 +0900

    feat: foodcategoryFragment ViewModel 수정

--- app/src/main/java/com/bestapp/rice/ui/foodcategory/FoodCategoryViewModel.kt
+++ app/src/main/java/com/bestapp/rice/ui/foodcategory/FoodCategoryViewModel.kt
@@ -1,5 +1,7 @@
 package com.bestapp.rice.ui.foodcategory
 
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bestapp.rice.data.model.remote.Filter
@@ -18,17 +20,25 @@ import kotlinx.coroutines.launch
 class FoodCategoryViewModel(
     private val meetingRepository: MeetingRepository,
     private val categoryRepository: CategoryRepository,
-    private val appSettingRepository: AppSettingRepository
+    private val appSettingRepository: AppSettingRepository,
+//    private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private val _meetingList = MutableSharedFlow<List<MeetingUiState>>(replay = 0)
+    private val _meetingList = MutableSharedFlow<List<MeetingUiState>>(replay = 1)
     val meetingList: SharedFlow<List<MeetingUiState>>
         get() = _meetingList
+//
+//    private val _foodCategory = MutableStateFlow<List<FilterUiState.FoodUiState>>(emptyList())
+//    val foodCategory: StateFlow<List<FilterUiState.FoodUiState>>
+//        get() = _foodCategory
+
+//    private lateinit var _foodCategory: MutableStateFlow<List<FilterUiState.FoodUiState>>
+//    val foodCategory: StateFlow<List<FilterUiState.FoodUiState>>
+//        get() = _foodCategory
 
 
-    private lateinit var _foodCategory: MutableStateFlow<List<FilterUiState.FoodUiState>>
-    val foodCategory: StateFlow<List<FilterUiState.FoodUiState>>
-        get() = _foodCategory
+    private val _foodCategory = MutableStateFlow<List<FilterUiState.FoodUiState>>(emptyList())
+    val foodCategory: StateFlow<List<FilterUiState.FoodUiState>> = _foodCategory
 
     private val _goMeetingNavi = MutableSharedFlow<GoMeetingNavi>(replay = 0)
     val goMeetingNavi: SharedFlow<GoMeetingNavi>
@@ -38,7 +48,13 @@ class FoodCategoryViewModel(
     val meetingDocumentID : String
         get() = _meetingDocumentID
 
+
+    private var _selectMenu = ""
+
     init {
+//        Log.e("cyc","init")
+//        Log.e("cyc","_selectMenu-->${_selectMenu}")
+
         viewModelScope.launch {
             runCatching {
                 categoryRepository.getFoodCategory()
@@ -49,7 +65,24 @@ class FoodCategoryViewModel(
                 _foodCategory.value = foodUiStateList
             }
         }
+//        savedStateHandle.get<FilterUiState.FoodUiState>("foodCategory")?.let {
+//            getFoodMeeting(it.name)
+//        }
     }
+
+
+//    init {
+//        viewModelScope.launch {
+//            runCatching {
+//                categoryRepository.getFoodCategory()
+//            }.onSuccess {
+//                val foodUiStateList = it.map { filter ->
+//                    FilterUiState.FoodUiState.createFrom(filter as Filter.Food)
+//                }
+//                _foodCategory = MutableStateFlow(foodUiStateList)
+//            }
+//        }
+//    }
 
 
     fun getFoodMeeting(mainMenu: String) {
@@ -57,7 +90,7 @@ class FoodCategoryViewModel(
             runCatching {
                 meetingRepository.getFoodMeeting(mainMenu)
             }.onSuccess {
-                val meetingUiStateList = it.map { meeting ->
+                val meetingUiStateList = it.map{meeting ->
                     MeetingUiState.createFrom(meeting)
                 }
                 _meetingList.emit(meetingUiStateList)
@@ -79,5 +112,15 @@ class FoodCategoryViewModel(
                 }
             }
         }
+    }
+
+    fun setSelectMenu(menu: String){
+        _selectMenu = menu
+        Log.e("cyc","아래_selectMenu-->${_selectMenu}")
+
+    }
+
+    fun getSelectMenu(): String{
+        return _selectMenu
     }
 }
