2a5995c277b6abee6caf71f7eebaacdf60ef0a1e
Choi YoonChan <choyc9239@gmail.com>
2024-06-04 22:55:35 +0900

    feat: FoodCategoryFragment custom tablayout 추가

--- app/src/main/java/com/bestapp/rice/ui/foodcategory/FoodCategoryFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/foodcategory/FoodCategoryFragment.kt
@@ -3,19 +3,18 @@ package com.bestapp.rice.ui.foodcategory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
-import coil.ImageLoader
-import coil.request.ImageRequest
-import com.bestapp.rice.R
+import coil.load
 import com.bestapp.rice.databinding.FragmentFoodCategoryBinding
+import com.bestapp.rice.databinding.MenuFoodCategoryBinding
 import com.bestapp.rice.ui.BaseFragment
 import com.bestapp.rice.ui.foodcategory.viewpager.FoodCategoryViewpagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 
@@ -30,7 +29,6 @@ class FoodCategoryFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupData()
         setupListener()
         setupObserve()
         setupView()
@@ -41,15 +39,27 @@ class FoodCategoryFragment :
             FoodCategoryViewpagerAdapter(childFragmentManager, lifecycle)
         binding.vp.adapter = foodCategoryViewpagerAdapter
 
-    }
+        TabLayoutMediator(binding.tl, binding.vp) { tab, position ->
 
-    private fun setupData() {
+            val mBinding = MenuFoodCategoryBinding.inflate(layoutInflater, null, false)
 
+            lifecycleScope.launch {
+                tab.text = viewModel.foodCategory.first()[position].name
+                val icon = viewModel.foodCategory.first()[position].icon
+                tab.customView = (createTabItemView(mBinding, tab.text.toString(), icon))
+                tab.customView?.setOnClickListener {
+                    mBinding.tv.isSelected = !mBinding.tv.isSelected
+                    binding.tl.selectTab(tab)
+                    viewModel.selectIndex = position
+                }
+            }
+        }.attach()
     }
 
     private fun setupListener() {
         binding.tl.onTabSelected { tab ->
             if (tab.text.isNullOrEmpty()) {
+                Log.e("cyc", "")
                 return@onTabSelected
             }
             viewModel.selectMenu = tab.text.toString()
@@ -67,32 +77,10 @@ class FoodCategoryFragment :
                     it.forEachIndexed { index, foodUiState ->
                         foodCategoryViewpagerAdapter.addFragment(InputFoodCategoryFragment())
                     }
+
                     // tablayout
                     // inline 해당 지역 return 확인 해보기
                     foodCategoryViewpagerAdapter.notifyDataSetChanged()
-                    TabLayoutMediator(binding.tl, binding.vp) { tab, position ->
-                        tab.text = it[position].name
-                        val tabIcon = ContextCompat.getDrawable(
-                            requireContext(),
-                            R.drawable.ic_launcher_background
-                        )
-                        // 해당 이미지를 tab Icon에 넣는 작업
-                        val request = ImageRequest.Builder(requireContext())
-                            .data(it[position].icon)
-                            .target { drawable ->
-                                Log.e("cyc","drawable-->${drawable}")
-                                tab.setIcon(drawable)
-                            }
-                            .error(tabIcon)
-                            .build()
-
-                        //로드되는 Builder
-                        val imageLoader = ImageLoader.Builder(requireContext())
-                            .crossfade(true)
-                            .build()
-                        imageLoader.enqueue(request)
-                        Log.e("cyc","로드 완료")
-                    }.attach()
                     binding.vp.setCurrentItem(viewModel.selectIndex, true)
                 }
             }
@@ -123,6 +111,16 @@ class FoodCategoryFragment :
                 }
             }
         }
+    }
+
+    private fun createTabItemView(
+        mBinding: MenuFoodCategoryBinding,
+        text: String,
+        imgUri: String
+    ): View {
+        mBinding.iv.load(imgUri)
+        mBinding.tv.text = text
+        return mBinding.root
     }
 }
 
