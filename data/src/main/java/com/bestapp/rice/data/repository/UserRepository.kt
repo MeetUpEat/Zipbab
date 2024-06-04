b2fe99c291f2c9aab515afdd691ef71f7412b8cb
rlaxodud214 <rlaxodud214@naver.com>
2024-06-05 01:20:13 +0900

    feat(UserRepository): 업데이트 처리 결과를 반환하도록 수정
    feat: 중복된 코드 메서드로 분리
    
    fix: 전체 메서드 테스트 및 오류 수정

--- data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/UserRepository.kt
@@ -9,12 +9,12 @@ import com.bestapp.rice.data.model.remote.User
 interface UserRepository {
     suspend fun getUser(userDocumentId: String): User
     suspend fun login(id: String, pw: String): Boolean
-    suspend fun signUpUser(user: User)
-    suspend fun signOutUser(userDocumentId: String)
-    suspend fun updateUserNickname(userDocumentId: String, nickname: String)
-    suspend fun updateUserTemperature(reviews: List<Review>)
-    suspend fun updateUserMeetingCount(userDocumentID: String)
-    suspend fun updateUserProfileImage(userDocumentID: String, profileImageUri: String?)
+    suspend fun signUpUser(user: User): Boolean
+    suspend fun signOutUser(userDocumentId: String): Boolean
+    suspend fun updateUserNickname(userDocumentId: String, nickname: String): Boolean
+    suspend fun updateUserTemperature(reviews: List<Review>): Boolean
+    suspend fun updateUserMeetingCount(userDocumentID: String): Boolean
+    suspend fun updateUserProfileImage(userDocumentID: String, profileImageUri: String?): Boolean
     suspend fun convertImages(userDocumentID: String, images: List<Bitmap>): List<String>
     suspend fun addPost(userID: String, post: Post): Boolean
 }
--- data/src/main/java/com/bestapp/rice/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/bestapp/rice/data/repository/UserRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.bestapp.rice.data.repository
 
 import android.graphics.Bitmap
 import android.net.Uri
-import com.bestapp.rice.data.model.remote.PlaceLocation
+import com.bestapp.rice.data.doneSuccessful
 import com.bestapp.rice.data.model.remote.Post
 import com.bestapp.rice.data.model.remote.Review
 import com.bestapp.rice.data.model.remote.User
@@ -42,49 +42,60 @@ class UserRepositoryImpl(
         return false
     }
 
-    override suspend fun signUpUser(user: User) {
-        val userDocumentRef = userDB.add(user).await()
+    override suspend fun signUpUser(user: User): Boolean {
+        val userDocumentRef = userDB
+            .add(user)
+            .await()
         val userDocumentId = userDocumentRef.id
 
-        userDB.document(userDocumentId)
+        return userDB.document(userDocumentId)
             .update("userDocumentId", userDocumentId)
-            .await()
+            .doneSuccessful()
     }
 
-    override suspend fun signOutUser(userDocumentId: String) {
-        userDB.document(userDocumentId)
+    override suspend fun signOutUser(userDocumentId: String): Boolean {
+        return userDB.document(userDocumentId)
             .delete()
-            .await()
+            .doneSuccessful()
     }
 
-    override suspend fun updateUserNickname(userDocumentId: String, nickname: String) {
-        userDB.document(userDocumentId)
+    override suspend fun updateUserNickname(userDocumentId: String, nickname: String): Boolean {
+        return userDB.document(userDocumentId)
             .update("nickname", nickname)
-            .await()
+            .doneSuccessful()
     }
-    override suspend fun updateUserTemperature(reviews: List<Review>) {
+
+    override suspend fun updateUserTemperature(reviews: List<Review>): Boolean {
+        val isSuccessfuls = Array<Boolean>(8) { false }
+        var index = 0
+
         reviews.forEach { (id, votingPoint) ->
-            userDB.document(id)
+            isSuccessfuls[index++] = userDB.document(id)
                 .update("temperature", FieldValue.increment(votingPoint))
-                .await()
+                .doneSuccessful()
         }
+
+        return isSuccessfuls.all { it == true }
     }
 
-    override suspend fun updateUserMeetingCount(userDocumentID: String) {
-        userDB.document(userDocumentID)
+    override suspend fun updateUserMeetingCount(userDocumentID: String): Boolean {
+        return userDB.document(userDocumentID)
             .update("meetingCount", FieldValue.increment(1))
-            .await()
+            .doneSuccessful()
     }
 
-    override suspend fun updateUserProfileImage(userDocumentID: String, profileImageUri: String?) {
-        // TODO: null일 때, 로직 수정 필요
-        val uri = storageRepositoryImpl.uploadImage(
-            Uri.parse(profileImageUri ?: FAKE_USER.profileImage)
-        )
+    override suspend fun updateUserProfileImage(userDocumentID: String, profileImageUri: String?): Boolean {
+        val uri = if (profileImageUri.isNullOrBlank()) {
+            USER_DEFAULT_IMAGE_URI
+        } else {
+            storageRepositoryImpl.uploadImage(
+                Uri.parse(profileImageUri)
+            )
+        }
 
-        userDB.document(userDocumentID)
+        return userDB.document(userDocumentID)
             .update("profileImage", uri)
-            .await()
+            .doneSuccessful()
     }
 
     override suspend fun convertImages(userDocumentID: String, images: List<Bitmap>): List<String> {
@@ -99,15 +110,14 @@ class UserRepositoryImpl(
 
     override suspend fun addPost(userDocumentID: String, post: Post): Boolean {
         return userDB.document(userDocumentID)
-            .update(
-                "posts",
-                FieldValue.arrayUnion(post)
-            ).isSuccessful
+            .update("posts", FieldValue.arrayUnion(post))
+            .doneSuccessful()
     }
 
     companion object {
         private val storageRepositoryImpl = StorageRepositoryImpl()
 
         private val FAKE_USER = User()
+        private val USER_DEFAULT_IMAGE_URI = "https://firebasestorage.googleapis.com/v0/b/food-879fc.appspot.com/o/images%2F1717515927323.jpg?alt=media&token=026118a6-50ff-4add-a371-5d7f7feda46c"
     }
 }
