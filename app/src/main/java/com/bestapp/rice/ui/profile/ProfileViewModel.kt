d912fb9cac165fcfceec99a4f76cdfabf90b22f1
Choi YoonChan <choyc9239@gmail.com>
2024-06-06 20:44:37 +0900

    feat: ProfileViewModel 힐트 적용

--- app/src/main/java/com/bestapp/rice/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/bestapp/rice/ui/profile/ProfileViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.bestapp.rice.data.repository.AppSettingRepository
 import com.bestapp.rice.data.repository.UserRepository
 import com.bestapp.rice.model.toUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class ProfileViewModel @Inject constructor(
     private val userRepository: UserRepository,
     private val appSettingRepository: AppSettingRepository,
