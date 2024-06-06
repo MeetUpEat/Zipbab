94eb3c600e3c43f49a69f7e6c18b6c388f8c55b9
DoDoBest <send1684@gmail.com>
2024-06-06 16:26:30 +0900

    chore: 코드 정리

--- app/src/main/java/com/bestapp/rice/ui/profileedit/ProfileEditViewModelFactory.kt
+++ app/src/main/java/com/bestapp/rice/ui/profileedit/ProfileEditViewModelFactory.kt
@@ -6,13 +6,13 @@ import com.bestapp.rice.data.network.FirebaseClient
 import com.bestapp.rice.data.repository.UserRepository
 import com.bestapp.rice.data.repository.UserRepositoryImpl
 
-class ProfileEditViewModelFactory: ViewModelProvider.Factory {
+class ProfileEditViewModelFactory : ViewModelProvider.Factory {
 
     private val userRepository: UserRepository = UserRepositoryImpl(FirebaseClient.userStoreService)
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return when(modelClass) {
+        return when (modelClass) {
             ProfileEditViewModel::class.java -> ProfileEditViewModel(userRepository)
             else -> throw IllegalArgumentException()
         } as T
