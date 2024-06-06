6a99f3675af5cfe1004fc5089eddf4760bbb3c7b
Choi YoonChan <choyc9239@gmail.com>
2024-06-06 18:37:42 +0900

    feat: 모듈 Internal 추가

--- data/src/main/java/com/bestapp/rice/data/di/RepositoryModule.kt
+++ data/src/main/java/com/bestapp/rice/data/di/RepositoryModule.kt
@@ -20,7 +20,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
+internal abstract class RepositoryModule {
 
     @Binds
     @Singleton
