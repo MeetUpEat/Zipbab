2ab59374bb0d91b0adeb019afe77fad66011ceb3
rlaxodud214 <rlaxodud214@naver.com>
2024-06-08 00:46:51 +0900

    test: Map 사용을 위한 기본 코드 셋업, 401 인증 오류 발생중

--- app/src/main/java/com/bestapp/rice/ui/meetupmap/MeetUpMapFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/meetupmap/MeetUpMapFragment.kt
@@ -1,11 +1,18 @@
 package com.bestapp.rice.ui.meetupmap
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bestapp.rice.databinding.FragmentMeetUpMapBinding
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMap.OnMapViewInfoChangeListener
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapType
+import com.kakao.vectormap.MapViewInfo
 
 class MeetUpMapFragment : Fragment() {
 
@@ -13,19 +20,73 @@ class MeetUpMapFragment : Fragment() {
     private val binding: FragmentMeetUpMapBinding
         get() = _binding!!
 
+    val mapLifeCycleCallback = object : MapLifeCycleCallback() {
+        // 지도 API 가 정상적으로 종료될 때 호출됨
+        override fun onMapDestroy() {
+            Log.e(TAG, "onMapDestroy")
+        }
+
+        // 지도 API 가 정상적으로 종료될 때 호출됨
+        override fun onMapError(p0: Exception?) {
+            Log.e(TAG, "onMapError")
+        }
+    }
+
+    val kakaoMapReadyCallback = object : KakaoMapReadyCallback() {
+        // 인증 후 API 가 정상적으로 실행될 때 호출됨
+        override fun onMapReady(kakaoMap: KakaoMap) {
+            Log.e(TAG, "onMapReady")
+
+            kakaoMap.changeMapViewInfo(MapViewInfo.from("openmap", MapType.NORMAL));
+
+            kakaoMap.setOnMapViewInfoChangeListener(object : OnMapViewInfoChangeListener {
+                // MapType 변경 성공 시 호출
+                override fun onMapViewInfoChanged(mapViewInfo: MapViewInfo) {
+                    Log.e(TAG, "onMapViewInfoChanged")
+                }
+
+                // MapType 변경 실패 시 호출
+                override fun onMapViewInfoChangeFailed() {
+                    Log.e(TAG, "onMapViewInfoChangeFailed")
+                }
+            })
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentMeetUpMapBinding.inflate(inflater, container, false)
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mv.start(mapLifeCycleCallback, kakaoMapReadyCallback)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mv.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mv.pause()
+    }
+
     override fun onDestroyView() {
         _binding = null
 
         super.onDestroyView()
+    }
+
+    companion object {
+        const val TAG = "KakaoMap lifecycle 테스트"
     }
 }
