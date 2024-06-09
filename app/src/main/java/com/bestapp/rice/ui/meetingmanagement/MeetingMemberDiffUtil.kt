bc18fdaeb3890590834ee02e5466a65222e583fd
Choi YoonChan <choyc9239@gmail.com>
2024-06-09 21:08:38 +0900

    comment: MeetingMemberDiffUtil의 불필요 주석 삭제

--- app/src/main/java/com/bestapp/rice/ui/meetingmanagement/MeetingMemberDiffUtil.kt
+++ app/src/main/java/com/bestapp/rice/ui/meetingmanagement/MeetingMemberDiffUtil.kt
@@ -1,7 +1,6 @@
 package com.bestapp.rice.ui.meetingmanagement
 
 import androidx.recyclerview.widget.DiffUtil
-import com.bestapp.rice.model.FilterUiState
 import com.bestapp.rice.model.UserUiState
 
 class MeetingMemberDiffUtil : DiffUtil.ItemCallback<UserUiState>() {
