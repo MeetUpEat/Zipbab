e497419df00bde6b61fa6f20fa0a3907d8ffdfc7
Choi YoonChan <choyc9239@gmail.com>
2024-06-09 21:07:23 +0900

    feat: MeetingMemberViewHolder의 값 세팅 및 리스너 추가

--- app/src/main/java/com/bestapp/rice/ui/meetingmanagement/MeetingMemberViewHolder.kt
+++ app/src/main/java/com/bestapp/rice/ui/meetingmanagement/MeetingMemberViewHolder.kt
@@ -5,27 +5,26 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.bestapp.rice.R
-import com.bestapp.rice.databinding.ItemFoodMenuBinding
-import com.bestapp.rice.model.FilterUiState
+import com.bestapp.rice.databinding.ItemMemberBinding
 import com.bestapp.rice.model.UserUiState
 
 class MeetingMemberViewHolder(
     parent: ViewGroup,
-    private val onCostClick: (userUiState: UserUiState) -> Unit,
+    private val onMemberClick: (userUiState: UserUiState) -> Unit,
 ) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_cost, parent, false)
 ) {
 
-    private val binding = ItemFoodMenuBinding.bind(itemView)
+    private val binding = ItemMemberBinding.bind(itemView)
 
     fun bind(
         userUiState: UserUiState
     ) {
 
-//        binding.iv.load(userUiState)
-//        binding.tv.text = userUiState.name
-//        binding.root.setOnClickListener {
-//            onCostClick(userUiState)
-//        }
+        binding.ivImg.load(userUiState.profileImage)
+        binding.tvMembserName.text = userUiState.nickname
+        binding.root.setOnClickListener {
+            onMemberClick(userUiState)
+        }
     }
 }
