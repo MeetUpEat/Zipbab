04a7eaeb615b073fc1d8b616b56fc86c8d8d27d6
Choi YoonChan <choyc9239@gmail.com>
2024-06-10 10:53:12 +0900

    feat: costFragment 코드 추가

--- app/src/main/java/com/bestapp/rice/ui/cost/CostFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/cost/CostFragment.kt
@@ -5,14 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.bestapp.rice.databinding.FragmentCostBinding
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class CostFragment : Fragment() {
 
     private var _binding: FragmentCostBinding? = null
     private val binding
         get() = _binding!!
+
+    private val viewModel: CostViewModel by viewModels()
+
+    private lateinit var costCategoryViewpagerAdapter: CostCategoryViewpagerAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,9 +38,88 @@ class CostFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+        setupObserve()
+        setupView()
+    }
+
+    private fun setupView() {
+        costCategoryViewpagerAdapter =
+            CostCategoryViewpagerAdapter(childFragmentManager, lifecycle)
+        binding.vp.adapter = costCategoryViewpagerAdapter
+
+        TabLayoutMediator(binding.tl, binding.vp) { tab, position ->
+            lifecycleScope.launch {
+                tab.text = viewModel.costCategory.first()[position].name
+            }
+        }.attach()
+    }
+
+    private fun setupObserve() {
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.costCategory.collect {
+                    it.ifEmpty { return@collect }
+
+                    it.forEach { costUiState ->
+                        costCategoryViewpagerAdapter.addFragment(InputCostFragment())
+                    }
+
+                    costCategoryViewpagerAdapter.notifyDataSetChanged()
+                    binding.vp.setCurrentItem(viewModel.selectIndex, true)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.goMeetingNavi.collect { goMeetingNavi ->
+                    when (goMeetingNavi.first) {
+                        MoveMeetingNavi.GO_MEETING_INFO -> {
+                            val action =
+                                CostFragmentDirections.actionCostFragmentToMeetingInfoFragment(
+                                    goMeetingNavi.second
+                                )
+                            findNavController().navigate(action)
+                        }
+
+                        MoveMeetingNavi.GO_MEETING_MANAGEMENT -> {
+                            val action =
+                                CostFragmentDirections.actionCostFragmentToMeetingManagementFragment(
+                                    goMeetingNavi.second
+                                )
+                            findNavController().navigate(action)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupListener() {
+        binding.tl.onTabSelected { tab ->
+            if (tab.text.isNullOrEmpty()) {
+                return@onTabSelected
+            }
+            viewModel.selectTab(tab.text.toString(), tab.position)
+//            viewModel.selectCost = tab.text.toString()
+//            viewModel.selectIndex = tab.position
+//            viewModel.getCostMeeting(viewModel.selectCost)
+        }
+        binding.mt.setNavigationOnClickListener {
+            if (!findNavController().popBackStack()) {
+                requireActivity().finish()
+            }
+        }
+    }
+
     override fun onDestroyView() {
         _binding = null
-
+        binding.vp.adapter = null
         super.onDestroyView()
     }
 }
+
