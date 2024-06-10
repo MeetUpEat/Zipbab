e501770c315022cc8ec8b869140847d1c5b80dcd
DoDoBest <send1684@gmail.com>
2024-06-10 19:18:06 +0900

    fix: 누락된 conflict 해결

--- app/src/main/java/com/bestapp/rice/ui/profileimageselect/ProfileImageSelectFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/profileimageselect/ProfileImageSelectFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.lifecycleScope
@@ -13,7 +14,6 @@ import com.bestapp.rice.databinding.FragmentProfileImageSelectBinding
 import com.bestapp.rice.model.toArg
 import com.bestapp.rice.permission.ImagePermissionManager
 import com.bestapp.rice.permission.ImagePermissionType
-import com.bestapp.rice.util.setVisibility
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -122,7 +122,7 @@ class ProfileImageSelectFragment : Fragment() {
             binding.tvPermissionDescription,
             binding.tvRequestPermission
         ).map { view ->
-            view.setVisibility(isFullImageAccessGranted.not())
+            view.isVisible = isFullImageAccessGranted.not()
         }
         if (imagePermissionManager.isFullImageAccessGranted()) {
             imagePermissionManager.requestFullImageAccessPermission { images: List<GalleryImageInfo> ->
