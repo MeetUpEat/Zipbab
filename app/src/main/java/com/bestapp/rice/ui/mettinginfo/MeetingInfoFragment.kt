54e254325dd3238710e81b24b55c08f1a85d2776
yoonchanchoi <74814647+yoonchanchoi@users.noreply.github.com>
2024-06-13 21:32:10 +0900

    Feat/#42 모임 신청 페이지 UI와 기본 필요한 동작 구현 (#64)
    * feat: color 값 추가
    
    * feat: 클릭시 이벤트 enum class
    
    * feat: meeting Fragment 생성
    
    * feat: meeting Fragment 코드
    
    * feat: meeting ViewModel 추가
    
    * feat: meetingRepository 및 Impl getMeeting 함수 수정
    
    * feat: meetingInfo 필요 drawable 추가
    
    * feat: meetingInfo 필요 String xml 추가
    
    * feat: MeetingManagementFragment의 RecyclerView 삭제 및 수정
    
    * feat: MeetingInfoFragment 호스팅 유저 정보 세팅
    
    * feat: MeetingInfoFragment profile 이동 리너스 및 로직 세팅
    
    * feat: MeetingInfoFragment 백 버튼 처리
    
    * fix: string xml의 버그로 인한 값 수정
    
    * fix: 메인 메뉴 텍스트 값 크기에 따른 다른 style 적용
    
    * fix: 모임 신청 페이지 title 지정
    
    * fix: String xml 추가
    
    * fix: 모임의 1인당 가격 보여주는 변수 변경
    
    * fix: 버튼 drawable 추가 및 변경
    
    * fix: 신청 버튼아래 고정으로 변경
    
    * feat: 신청 버튼 연속 클릭 막기
    
    * comment: TODU주석 남기기
    
    * fix: MeetingInfo title 수정
    
    * feat: MeetinginfoFragment 호스트 타이틀 -> Strings xml 추가
    
    * fix: host -> hostDocumentId 변경
    
    * fix: checkLogin() 함수의 거꾸로 바뀐 로직 수정
    
    * fix: delay()를 통한 버튼 비활성화
    
    * fix: 같이 먹어요 color 수정
    
    * fix: FAKE_MEETING -> Exception으로 수정
    
    * fix: 호스트 정보 보여주는 로직 수정

--- app/src/main/java/com/bestapp/rice/ui/mettinginfo/MeetingInfoFragment.kt
+++ app/src/main/java/com/bestapp/rice/ui/mettinginfo/MeetingInfoFragment.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import coil.load
+import com.bestapp.rice.R
 import com.bestapp.rice.databinding.FragmentMeetingInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class MeetingInfoFragment : Fragment() {
 
     private var _binding: FragmentMeetingInfoBinding? = null
     private val binding: FragmentMeetingInfoBinding
         get() = _binding!!
+
+
+    private val viewModel: MeetingInfoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,9 +35,101 @@ class MeetingInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMeetingInfoBinding.inflate(inflater, container, false)
-
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObserve()
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.clHost.setOnClickListener {
+            val action = MeetingInfoFragmentDirections.actionMeetingInfoFragmentToProfileFragment(viewModel.hostDocumentId)
+            findNavController().navigate(action)
+        }
+
+        binding.btn.setOnClickListener {
+            viewModel.btnEvent()
+        }
+        binding.mt.setNavigationOnClickListener {
+            if (!findNavController().popBackStack()) {
+                requireActivity().finish()
+            }
+        }
+    }
+
+    private fun setupObserve() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.meeting.collect { meetingUiState ->
+                    binding.iv.load(meetingUiState.titleImage)
+                    binding.tvTitle.text = meetingUiState.title
+                    binding.tvPeopleCount.text = String.format(
+                        resources.getString(R.string.meeting_info_count),
+                        meetingUiState.members.size
+                    )
+                    binding.tvLocation.text = String.format(
+                        resources.getString(R.string.meeting_info_location),
+                        meetingUiState.placeLocationUiState.locationAddress
+                    )
+                    binding.tvTime.text = String.format(
+                        resources.getString(R.string.meeting_info_time),
+                        meetingUiState.time
+                    )
+                    binding.tvMainMenu.text = String.format(
+                        resources.getString(R.string.meeting_info_main_menu),
+                        meetingUiState.mainMenu
+                    )
+                    binding.tvCost.text = String.format(
+                        resources.getString(
+                            R.string.meeting_info_cost,
+                            meetingUiState.costValueByPerson
+                        )
+                    )
+                    binding.tvContent.text = meetingUiState.description
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isPossible.collect {
+                    binding.btn.isEnabled = it
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.hostUser.collect {
+                    binding.ivImg.load(it.profileImage)
+                    binding.tvHostName.text = it.nickname
+                }
+            }
+        }
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.event.collect {
+                    when (it) {
+                        Event.GO_EVENT -> {
+                            findNavController().navigate(R.id.action_meetingInfoFragment_to_loginFragment)
+                        }
+
+                        Event.JOIN_MEETING -> {
+                            viewModel.addPendingMember()
+                            Toast.makeText(requireActivity(), "신청되셨습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    binding.btn.isEnabled  = false
+                    delay(3000)
+                    binding.btn.isEnabled = true
+                }
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         _binding = null
