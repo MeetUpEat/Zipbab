aada0cbdb67950a45c359af011bcc1cb3c826d80
DoDoBest <send1684@gmail.com>
2024-06-13 16:38:37 +0900

    feat: SplashScreen API 설정

--- app/src/main/java/com/bestapp/rice/ui/MainActivity.kt
+++ app/src/main/java/com/bestapp/rice/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MotionEvent
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
