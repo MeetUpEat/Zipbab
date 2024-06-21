b3344ca6500afc781871b7f774b89b48e045e16f
DoDoBest <send1684@gmail.com>
2024-06-22 01:11:18 +0900

    build: data 모듈에 paging 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.paging.runtime)
 
     // retrofit
     implementation(libs.retrofit)
